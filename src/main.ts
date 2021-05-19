43286ddf27017da81ff3a37839c75558c9d609aa
zeinab hajhosseini <zeinab.hajhosseini72@gmail.com>
2021-05-19 18:29:19 +0430

    Config Swagger Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Config for Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('server-app')
+    .setDescription('Back-end side of project management system')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/doc', app, document);
+
+  await app.listen(3000, ()=>{
+    console.log('Server Run On Port 3000');
+  });
 }
 bootstrap();
