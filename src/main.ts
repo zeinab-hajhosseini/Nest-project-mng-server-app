174707c0b21b679ceefb9d52b4427bd8800e8f01
zeinab hajhosseini <zeinab.hajhosseini72@gmail.com>
2021-05-20 06:42:26 +0430

    Add General validator pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe);
 
   // Config for Swagger Documentation
   const config = new DocumentBuilder()
--- src/modules/employees/dto/create-employee.dto.ts
+++ src/modules/employees/dto/create-employee.dto.ts
@@ -1,25 +1,30 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsPostalCode, IsString, MinLength } from "class-validator";
 
 export class CreateEmployeeDto {
 
+    @IsString()
+    @MinLength(2)
     @ApiProperty({description: 'FirstName'})
     FirstName: string;
 
+    @IsString()
     @ApiProperty({description: 'LastName'})
     LastName: string;
-
 
     @ApiProperty({description: 'Gender: Male or Female'})
     Gender: string;
 
+    @IsDate()
     @ApiProperty({description: 'Birth day Date'})
     Birthdate: Date;
 
     @ApiProperty({description: 'City of Address'})
     City: string;
 
+    @IsPostalCode()
     @ApiProperty({description: 'PostalCode of Address'})
-    PostalCode: number;
+    PostalCode: string;
 
     @ApiProperty({description: 'Street of Address'})
     Street: string;
